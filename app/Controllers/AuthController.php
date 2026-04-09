b0a793d29b390f1a7e558e0dc4ad1ed55f2774f2
ilyasaitbamou <ilyasaitbamou2005@gmail.com>
2026-04-09 11:38:13 +0100

    setup routing

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+class AuthController {
+    public function login() {
+        // Login logic here
+    }
+
+    public function signup() {
+        // Signup logic here
+    }
+
+    public function logout() {
+        // Logout logic here
+    }
+
+}
